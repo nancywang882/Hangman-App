6e55a5587e37f968bda88b445a7e8d480b65d59e
Nancy Wang <nancywang882@gmail.com>
2018-01-08 23:44:42 -0500

    fixed buttons

--- app/src/main/java/com/example/nancy/hangman/DisplayMessageActivity.java
+++ app/src/main/java/com/example/nancy/hangman/DisplayMessageActivity.java
@@ -27,6 +27,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
     private String currWord;
     private LinearLayout wordLayout;
     private TextView[] charViews;
+    //body part images
+    private ImageView[] bodyParts;
+    //number of body parts
+    private int numParts=6;
+    //current part - will increment when wrong answers are chosen
+    private int currPart;
+    //number of characters in current word
+    private int numChars;
+    //number correctly guessed
+    private int numCorr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +48,27 @@ public class DisplayMessageActivity extends AppCompatActivity {
         currWord = "";
         wordLayout = (LinearLayout)findViewById(R.id.word);
         letters = (GridView)findViewById(R.id.letters);
+        bodyParts = new ImageView[numParts];
+        bodyParts[0] = (ImageView)findViewById(R.id.head);
+        bodyParts[1] = (ImageView)findViewById(R.id.body);
+        bodyParts[2] = (ImageView)findViewById(R.id.arm1);
+        bodyParts[3] = (ImageView)findViewById(R.id.arm2);
+        bodyParts[4] = (ImageView)findViewById(R.id.leg1);
+        bodyParts[5] = (ImageView)findViewById(R.id.leg2);
+        for(int p = 0; p < numParts; p++) {
+            bodyParts[p].setVisibility(View.INVISIBLE);
+        }
         playGame();
     }
     private void playGame() {
+        currPart=0;
+
+        numCorr=0;
+
         String newWord = words[rand.nextInt(words.length)];
         while(newWord.equals(currWord)) newWord = words[rand.nextInt(words.length)];
         currWord = newWord;
+        numChars=currWord.length();
         charViews = new TextView[currWord.length()];
         wordLayout.removeAllViews();
         for (int c = 0; c < currWord.length(); c++) {
@@ -60,17 +85,84 @@ public class DisplayMessageActivity extends AppCompatActivity {
         letters.setAdapter(ltrAdapt);
         //play a new game
     }
-    //body part images
-    private ImageView[] bodyParts;
-    //number of body parts
-    private int numParts=6;
-    //current part - will increment when wrong answers are chosen
-    private int currPart;
-    //number of characters in current word
-    private int numChars;
-    //number correctly guessed
-    private int numCorr;
+
 
     private GridView letters;
     private LetterAdapter ltrAdapt;
+
+    public void letterPressed(View view) {
+        String ltr = ((TextView) view).getText().toString();
+        char letterChar = ltr.charAt(0);
+        view.setEnabled(false);
+        view.setBackgroundResource(R.drawable.letter_down);
+        boolean correct = false;
+        for (int k = 0; k < currWord.length(); k++) {
+            if (currWord.charAt(k) == letterChar) {
+                correct = true;
+                numCorr++;
+                charViews[k].setTextColor(Color.BLACK);
+            }
+        }
+        if (correct) {
+            if (numCorr == numChars) {
+                // Disable Buttons
+                disableBtns();
+
+                // Display Alert Dialog
+                AlertDialog.Builder winBuild = new AlertDialog.Builder(this);
+                winBuild.setTitle("YAY");
+                winBuild.setMessage("You win!\n\nThe answer was:\n\n" + currWord);
+                winBuild.setPositiveButton("Play Again",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                DisplayMessageActivity.this.playGame();
+                            }
+                        });
+
+                winBuild.setNegativeButton("Exit",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                DisplayMessageActivity.this.finish();
+                            }
+                        });
+
+                winBuild.show();
+            }
+        } else if (currPart < numParts) {
+            //some guesses left
+            bodyParts[currPart].setVisibility(View.VISIBLE);
+            currPart++;
+        } else {
+            //user has lost
+            disableBtns();
+
+            // Display Alert Dialog
+            AlertDialog.Builder loseBuild = new AlertDialog.Builder(this);
+            loseBuild.setTitle("OOPS");
+            loseBuild.setMessage("You lose!\n\nThe answer was:\n\n" + currWord);
+            loseBuild.setPositiveButton("Play Again",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            DisplayMessageActivity.this.playGame();
+                        }
+                    });
+
+            loseBuild.setNegativeButton("Exit",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            DisplayMessageActivity.this.finish();
+                        }
+                    });
+
+            loseBuild.show();
+        }
+    }
+
+    public void disableBtns() {
+        int numLetters = letters.getChildCount();
+        for (int l = 0; l < numLetters; l++) {
+            letters.getChildAt(l).setEnabled(false);
+
+        }
+    }
 }
