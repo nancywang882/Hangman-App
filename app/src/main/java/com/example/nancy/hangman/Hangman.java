b206daddf90d5f2959ecc0219e4062e92ff96e28
Nancy Wang <nancywang882@gmail.com>
2018-01-08 21:23:43 -0500

    second commit

--- app/src/main/java/com/example/nancy/hangman/Hangman.java
+++ app/src/main/java/com/example/nancy/hangman/Hangman.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View.OnClickListener;
-
+//food
 public class Hangman extends AppCompatActivity implements OnClickListener{
     @Override
     public void onClick(View view) {
